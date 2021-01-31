955c6ebec4426785cc23d3cb40c028b4e299a82b
ZiggyHowland <sigbjorn@hovland.nu>
2021-01-31 19:47:23 +0100

    Added functionality in main main() class and in each students package.

--- src/dnb/reskill/Main.java
+++ src/dnb/reskill/Main.java
@@ -3,7 +3,16 @@ package dnb.reskill;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("This is the Main function in dnb.reskill. There is one package per student.");
+        System.out.println("This is the Main function in dnb.reskill. There is one package per 'student'.");
+        System.out.println("----");
+        dnb.reskill.arild.Main.main(args);
+        dnb.reskill.knut.Main.main(args);
+        dnb.reskill.marina.Main.main(args);
+        dnb.reskill.petter.Main.main(args);
+        dnb.reskill.salim.Main.main(args);
+        dnb.reskill.sigbjorn.Main.main(args);
+        dnb.reskill.svetlana.Main.main(args);
+        dnb.reskill.tom.Main.main(args);
+
     }
 }
