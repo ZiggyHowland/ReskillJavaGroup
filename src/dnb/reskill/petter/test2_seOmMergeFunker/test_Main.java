cb7b2cdfb29f44b3b76721383e1957c4ded42dfc
Petter Tondel <petter.toendel@outlook.com>
2021-02-02 14:36:24 +0100

    Tester opprettelse av Branch og Merge

---
+++ src/dnb/reskill/petter/test2_seOmMergeFunker/test_Main.java
@@ -0,0 +1,7 @@
+package dnb.reskill.petter.test2_seOmMergeFunker;
+
+public class test_Main {
+    public static void main(String[] args) {
+        System.out.println("Hvis noen andre enn meg ser dette, fungerte opprettelse av ny branch og merge");
+    }
+}
