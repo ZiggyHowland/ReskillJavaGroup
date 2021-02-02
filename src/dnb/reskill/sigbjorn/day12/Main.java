78d0b70397f433427c0fefc6f517ce15f00f4004
Sigbjørn Hovland <sigbjorn@hovland.nu>
2021-02-02 07:52:09 +0100

    Start of chapter 8 exercise 1. TBC

--- src/dnb/reskill/sigbjorn/day12/Main.java
+++ src/dnb/reskill/sigbjorn/day12/Main.java
@@ -4,13 +4,9 @@ package dnb.reskill.sigbjorn.day12;
 
 public class Main {
     public static void main(String[] args) {
-        //LessonCoding.run(); // Run the examples coded during lesson day 12
-        LabChapter7Exercise3.run();
+        //LabChapter7Exercise3.run();
 
     }
-
-
-
 
 }
 
